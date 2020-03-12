d74e164e5e4f3c6c788ec6ebe361d3dfa44004b7
Sangwon Hong <qpakzk@gmail.com>
2020-03-12 17:44:12 +0900

    update check and issue function

---
+++ src/main/java/kr/ac/postech/sslab/check/Check.java
@@ -0,0 +1,87 @@
+package kr.ac.postech.sslab.check;
+
+import kr.ac.postech.sslab.fabasset.chaincode.protocol.Extension;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+class Check {
+    private static final String CHECK_TYPE = "check";
+
+    private static final String BANK_KEY = "bank";
+
+    private static final String BALANCE_KEY = "balance";
+
+    private static final String SENDER_KEY = "sender";
+
+    private static final String DATE_AND_TIME_FORMAT = "yyyy/MM/dd HH:mm:ss";
+
+    boolean issue(ChaincodeStub stub, String bank, String issuer, long balance) throws IOException {
+        List<String> issuerIds = Extension.tokenIdsOf(stub, issuer, CHECK_TYPE);
+
+        boolean hasBank = false;
+        String id = null;
+        for (String issuerId: issuerIds) {
+            String whichBank = Extension.getXAttr(stub, issuerId, BANK_KEY);
+
+            if (whichBank == null) {
+                return false;
+            }
+
+            hasBank = whichBank.equals(bank);
+
+            if (hasBank) {
+                id = issuerId;
+                break;
+            }
+        }
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(DATE_AND_TIME_FORMAT);
+        LocalDateTime now = LocalDateTime.now();
+        String nowDataAndTime = dtf.format(now);
+
+        if (hasBank) {
+            String currentBalanceString = Extension.getXAttr(stub, id, BALANCE_KEY);
+            if (currentBalanceString == null) {
+                return false;
+            }
+
+            long currentBalance = Long.parseLong(currentBalanceString);
+            currentBalance += balance;
+            Extension.setXAttr(stub, id, BALANCE_KEY, Long.toString(currentBalance));
+
+            List<String> sender_info = new ArrayList<>();
+            sender_info.add(bank);
+            sender_info.add(Long.toString(balance));
+            sender_info.add(nowDataAndTime);
+            Extension.setXAttr(stub, id, SENDER_KEY, sender_info.toString());
+        }
+        else {
+            Map<String, Object> xattr = new HashMap<>();
+            xattr.put(BANK_KEY, bank);
+            xattr.put(BALANCE_KEY, balance);
+
+            xattr.put(SENDER_KEY, new ArrayList<>(Arrays.asList(bank, balance, nowDataAndTime)));
+
+            do {
+                long unixTime = Instant.now().getEpochSecond();
+                id = Long.toString(unixTime);
+            } while (stub.getStringState(id).length() == 0);
+
+            Extension.mint(stub, id, CHECK_TYPE, xattr, null);
+        }
+
+        return true;
+    }
+
+    boolean send(ChaincodeStub stub, String sender, String receiver, long balance) {
+        return true;
+    }
+
+    boolean redeem(ChaincodeStub stub, String redeemer, String bank, String account, long balance) {
+        return true;
+    }
+}
---
+++ src/main/java/kr/ac/postech/sslab/check/CheckMain.java
@@ -0,0 +1,94 @@
+package kr.ac.postech.sslab.check;
+
+import kr.ac.postech.sslab.fabasset.chaincode.Main;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ResponseUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+import static io.netty.util.internal.StringUtil.isNullOrEmpty;
+import static kr.ac.postech.sslab.fabasset.chaincode.constant.Message.ARG_MESSAGE;
+
+public class CheckMain extends Main {
+    private static final String ISSUE_FUNCTION_NAME = "issue";
+
+    private static final String SEND_FUNCTION_NAME = "send";
+
+    private static final String REDEEM_FUNCTION_NAME = "redeem";
+
+    private Check check = new Check();
+
+    @Override
+    public Response invoke(ChaincodeStub stub) {
+        try {
+            String func = stub.getFunction();
+            List<String> args = stub.getParameters();
+            String response;
+
+            switch (func) {
+                case ISSUE_FUNCTION_NAME:
+                    response = Boolean.toString(issue(stub, args));
+                    break;
+
+                case SEND_FUNCTION_NAME:
+                    response = Boolean.toString(send(stub, args));
+                    break;
+
+                case REDEEM_FUNCTION_NAME:
+                    response = Boolean.toString(redeem(stub, args));
+                    break;
+
+                default:
+                    return super.invoke(stub);
+            }
+
+            return ResponseUtils.newSuccessResponse(response);
+        } catch (Exception e) {
+            return ResponseUtils.newErrorResponse(e.getMessage());
+        }
+    }
+
+    private boolean issue(ChaincodeStub stub, List<String> args) throws IOException{
+        if (args.size() != 3 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "3"));
+        }
+
+        String bank = args.get(0);
+        String issuer = args.get(1);
+        String balance = args.get(2);
+
+        return check.issue(stub, bank, issuer, Long.parseLong(balance));
+    }
+
+    private boolean send(ChaincodeStub stub, List<String> args) {
+        if (args.size() != 3 || isNullOrEmpty(args.get(0))
+                || isNullOrEmpty(args.get(1)) || isNullOrEmpty(args.get(2))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "3"));
+        }
+
+        String sender = args.get(0);
+        String receiver = args.get(1);
+        String balance = args.get(2);
+
+        return check.send(stub, sender, receiver, Long.parseLong(balance));
+    }
+
+    private boolean redeem(ChaincodeStub stub, List<String> args) {
+        if (args.size() != 4 || isNullOrEmpty(args.get(0)) || isNullOrEmpty(args.get(1))
+                || isNullOrEmpty(args.get(2)) || isNullOrEmpty(args.get(3))) {
+            throw new IllegalArgumentException(String.format(ARG_MESSAGE, "4"));
+        }
+
+        String redeemer = args.get(0);
+        String bank = args.get(1);
+        String account = args.get(2);
+        String balance = args.get(3);
+
+        return check.redeem(stub, redeemer, bank, account, Long.parseLong(balance));
+    }
+    public static void main(String[] args) {
+        new CheckMain().start(args);
+    }
+}
